f5771e344badc0f2f00ae2f350bc866245e99eac
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2018-04-09 20:21:52 +0200

    Spaceship animation added

--- X-Multiply/ModulePlayer.cpp
+++ X-Multiply/ModulePlayer.cpp
@@ -19,11 +19,12 @@ ModulePlayer::ModulePlayer()
 	// go upward animation 
 	upward.PushBack({ 52, 0, 38, 16 });
 	upward.PushBack({ 4, 0, 38, 16 });
-	upward.PushBack({ 4, 0, 38, 10 });
+	upward.loop = false;
 	upward.speed = 0.06f;
 
 	downward.PushBack({ 148, 0, 38, 16 });
 	downward.PushBack({ 196, 0, 38, 16 });
+	downward.loop = false;
 	downward.speed = 0.06f;
 }
 
@@ -52,18 +53,30 @@ update_status ModulePlayer::Update()
 	}
 	if (App->input->keyboard[SDL_SCANCODE_W] == 1)
 	{
-		current_animation = &upward;
 		position.y -= speed;
+		current_animation = &upward;
+		downward.Reset();
+		
 	}
 	if (App->input->keyboard[SDL_SCANCODE_S] == 1)
 	{
-		current_animation = &downward;
 		position.y += speed;
+		current_animation = &downward;
+		upward.Reset();
+		
 	}
 	if (App->input->keyboard[SDL_SCANCODE_A] == 1)
 	{
 		position.x -= speed;
 	}
+
+	// Limits -------------------------------------
+	if (position.x <= 0) position.x = 0;
+	else if (position.x >= SCREEN_WIDTH - 35) position.x = SCREEN_WIDTH - 35;
+	if (position.y <= 12) position.y = 12;
+	else if (position.y >= SCREEN_HEIGHT ) position.y = SCREEN_HEIGHT ;
+
+
 	// Draw everything --------------------------------------
 	SDL_Rect r = current_animation->GetCurrentFrame();
 
