acdb4290a90a212eff5e04e165bf583900c3a540
JoanBarduena <36154195+JoanBarduena@users.noreply.github.com>
2018-04-21 18:01:33 +0200

    PlayerHitBox

--- X-Multiply/ModulePlayer.cpp
+++ X-Multiply/ModulePlayer.cpp
@@ -51,7 +51,7 @@ bool ModulePlayer::Start()
 	position.y = 120;
 	score = 0;
 
-	playerHitbox = App->collision->AddCollider({position.x , position.y, 32, 16}, COLLIDER_PLAYER, this);
+	playerHitbox = App->collision->AddCollider({ position.x, position.y, 7, 6 }, COLLIDER_PLAYER, this);
 
 	// Still have to load XMULTIPLY FONT
 	font_score = App->fonts->Load("fonts/rtype_font3.png", "! @,_./0123456789$;<&?abcdefghijklmnopqrstuvwxyz", 2);
@@ -173,7 +173,7 @@ update_status ModulePlayer::Update()
 			}
 		}
 		if (godmode == false)
-			playerHitbox->SetPos(position.x, position.y);
+			playerHitbox->SetPos(position.x + 9, position.y + 5);
 	}
 
 	//-----------------------------------------------------------------------------------------------------------------------
