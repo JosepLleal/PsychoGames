64f653da6e21fb9b2bc77fd1e3cddce680d97b6b
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2018-05-16 23:32:29 +0200

    Yellow Ball Animation improved

--- X-Multiply/Enemy_YellowBall.cpp
+++ X-Multiply/Enemy_YellowBall.cpp
@@ -13,7 +13,19 @@ Enemy_YellowBall::Enemy_YellowBall(int x, int y) : Enemy(x, y)
 	inflate.PushBack({ 66, 351, 29, 38 });
 	inflate.PushBack({ 101, 350, 29, 38 });
 	inflate.PushBack({ 133, 350, 29, 38 });
-	inflate.speed = 0.03f;
+
+	inflate.PushBack({ 34, 397, 29, 38 });
+	inflate.PushBack({ 66, 400, 29, 38 });
+	inflate.PushBack({ 66, 400, 29, 38 });
+	inflate.PushBack({ 66, 400, 29, 38 });
+	inflate.PushBack({ 66, 400, 29, 38 });
+	inflate.PushBack({ 66, 400, 29, 38 });
+
+	inflate.PushBack({ 98, 399, 29, 38 });
+	inflate.PushBack({ 130, 399, 29, 38 });
+
+
+	inflate.speed = 0.1f;
 	animation = &inflate;
 
 	collider = App->collision->AddCollider({ 0, 0, 29, 38 }, COLLIDER_TYPE::COLLIDER_REDBALL, (Module*)App->enemies);
@@ -24,13 +36,13 @@ Enemy_YellowBall::Enemy_YellowBall(int x, int y) : Enemy(x, y)
 
 void Enemy_YellowBall::Move()
 {
-	if (App->render->camera.x % 125 == 0)
+	if (App->render->camera.x % 136 == 0)
 	{
-		App->particles->AddParticle(App->particles->anemona_shot1, original_position.x + 24, original_position.y + 25, COLLIDER_ENEMY_SHOT);
-		App->particles->AddParticle(App->particles->anemona_shot2, original_position.x + 24, original_position.y + 15, COLLIDER_ENEMY_SHOT);
-		App->particles->AddParticle(App->particles->anemona_shot3, original_position.x + 24, original_position.y + 10, COLLIDER_ENEMY_SHOT);
-		App->particles->AddParticle(App->particles->anemona_shot4, original_position.x + 24, original_position.y + 20, COLLIDER_ENEMY_SHOT);
-		App->particles->AddParticle(App->particles->anemona_shot5, original_position.x + 24, original_position.y + 25, COLLIDER_ENEMY_SHOT);
+		App->particles->AddParticle(App->particles->anemona_shot1, original_position.x + 17, original_position.y + 15, COLLIDER_ENEMY_SHOT);
+		App->particles->AddParticle(App->particles->anemona_shot2, original_position.x + 17, original_position.y + 5, COLLIDER_ENEMY_SHOT);
+		App->particles->AddParticle(App->particles->anemona_shot3, original_position.x + 17, original_position.y + 0, COLLIDER_ENEMY_SHOT);
+		App->particles->AddParticle(App->particles->anemona_shot4, original_position.x + 17, original_position.y + 10, COLLIDER_ENEMY_SHOT);
+		App->particles->AddParticle(App->particles->anemona_shot5, original_position.x + 17, original_position.y + 15, COLLIDER_ENEMY_SHOT);
 	}
 }
 void Enemy_YellowBall::OnCollision(Collider* collider)
