8d81948fb25078542f5e382f09aac099849d3715
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2018-05-16 23:04:28 +0200

    Red Ball animation improved

--- X-Multiply/Enemy_Red_Ball.cpp
+++ X-Multiply/Enemy_Red_Ball.cpp
@@ -11,7 +11,7 @@ Enemy_Red_Ball::Enemy_Red_Ball(int x, int y) : Enemy(x, y)
 	animation = &one; 
 	one.speed = 0.0f;
 
-	life = 4;
+	life = 5;
 
 	original_position.x = x;
 	original_position.y = y;
@@ -26,39 +26,51 @@ Enemy_Red_Ball::Enemy_Red_Ball(int x, int y) : Enemy(x, y)
 void Enemy_Red_Ball::OnCollision(Collider* collider_)
 {
 
-	if (life == 4)
+	if (life == 5)
 	{
 		animation = &two;
 		two.PushBack({ 55, 113, 31, 31 });
 		two.speed = 0.0f;
 
+		position.x += 1;
+		position.y += 1;
+
 		collider->rect.h = 31;
 		collider->rect.w = 31;
 	}
 
-	else if (life == 3)
+	else if (life == 4)
 	{
 		animation = &three;
 		three.PushBack({ 90, 116, 26, 24 });
 		three.speed = 0.0f;
 
+		position.x += 3;
+		position.y += 4;
+
 		collider->rect.h = 24;
 		collider->rect.w = 26;
 	}
-	else if (life == 2)
+	else if (life == 3)
 	{
 		animation = &four;
 		four.PushBack({ 125, 119, 21, 19 });
 		four.speed = 0.0f;
 
+		position.x += 3;
+		position.y += 3;
+
 		collider->rect.h = 19;
 		collider->rect.w = 21;
 	}
-	else if (life == 1)
+	else if (life == 2)
 	{
 		animation = &five;
 		five.PushBack({ 160, 123, 15, 13 });
 		five.speed = 0.0f;
+
+		position.x += 3;
+		position.y += 3;
 
 		collider->rect.h = 13;
 		collider->rect.w = 15;
